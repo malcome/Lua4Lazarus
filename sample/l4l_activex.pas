efaf4b85acd0737cdf50ca16a6dfd8b1baae862b
malcome <malcome@users.noreply.github.com>
2015-02-12 17:47:08 +0900

    add event handling

--- sample/l4l_activex.pas
+++ sample/l4l_activex.pas
@@ -32,6 +32,41 @@ const
   FIELD_ID = '___IDispatch___';
   FIELD_FN = '___FuncName___';
   FIELD_ID_PARENT = '___IDispatchParent___';
+  FIELD_ID_CLIENT = '___IDispatchClient___';
+
+type
+
+  { TInterfacedBase }
+
+  TInterfacedBase = class(TObject, IUnknown)
+    function QueryInterface(constref iid : tguid; out obj) : longint; stdcall;
+    function _AddRef: Integer; stdcall;
+    function _Release: Integer; stdcall;
+  end;
+
+  { TEventSink }
+
+  TEventSink = class(TInterfacedBase, IDispatch)
+  private
+    L: PLua_State;
+    FCookie: DWORD;
+    FCPoint: IConnectionPoint;
+    FInfo: ITypeInfo;
+    EventList: TStringList;
+  public
+    constructor Create(aL: PLua_State);
+    destructor Destroy; override;
+    procedure AttachEvent(server: IDispatch);
+    procedure DetachEvent;
+    function IsEventSupport: boolean;
+    { IDispatch }
+    function GetTypeInfoCount(out Count: Integer): HRESULT; stdcall;
+    function GetTypeInfo({%H-}Index, {%H-}LocaleID: Integer; out {%H-}TypeInfo): HRESULT; stdcall;
+    function GetIDsOfNames(const {%H-}IID: TGUID; {%H-}Names: Pointer; {%H-}NameCount, {%H-}LocaleID: Integer; {%H-}DispIDs: Pointer): HRESULT; stdcall;
+    function Invoke(DispID: Integer; const {%H-}IID: TGUID; {%H-}LocaleID: Integer;
+                    {%H-}Flags: Word; var {%H-}Params; {%H-}VarResult, {%H-}ExcepInfo,
+                    {%H-}ArgErr: Pointer): HRESULT; stdcall;
+  end;
 
 function call(L : Plua_State) : Integer; cdecl; forward;
 procedure DoCreateActiveXObject(L : Plua_State; id: IDispatch); forward;
@@ -234,6 +269,8 @@ var
   di, diput: TDispID;
   param: TDispParams;
   v: OleVariant;
+  i: integer;
+  sink: ^TEventSink;
 begin
   Result:=0;
   lua_getmetatable(L, 1);
@@ -251,15 +288,48 @@ begin
   end else begin
     id:= IDispatch(TVarData(p^).vDispatch);
     ws:= UTF8Decode(key);
-    ChkErr(L, id.GetIDsOfNames(GUID_NULL, @ws, 1, GetUserDefaultLCID, @di), key);
-    Lua2Var(L, @v, 3);
-    diput := DISPID_PROPERTYPUT;
-    param.rgvarg := @v;
-    param.cArgs := 1;
-    param.rgdispidNamedArgs := @diput;
-    param.cNamedArgs := 1;
-    ChkErr(L, id.Invoke(di, GUID_NULL, GetUserDefaultLCID,
-     DISPATCH_PROPERTYPUT, param, nil, nil, nil), key);
+    if lua_isfunction(L, 3) or lua_isnil(L, 3) then begin
+      // Set event
+      lua_getmetatable(L, 1);
+      lua_getfield(L, -1, FIELD_ID_CLIENT);
+      sink:= lua_touserdata(L, -1);
+      lua_pop(L, 2);
+      key:= LowerCase(key);
+      if lua_isfunction(L, 3) then begin
+        i:= sink^.EventList.IndexOf(key);
+        if i >= 0 then begin
+          // Release old function from REGISTRY
+          luaL_unref(L, LUA_REGISTRYINDEX, Integer(sink^.EventList.Objects[i]));
+          // Save new function to REGISTRY
+          lua_pushvalue(L, 3);
+          sink^.EventList.Objects[i]:= TObject(luaL_Ref(L, LUA_REGISTRYINDEX));
+        end else begin
+          if sink^.EventList.Count = 0 then
+            sink^.AttachEvent(id); // Start event receive
+          // Save new function to REGISTRY
+          lua_pushvalue(L, 3);
+          sink^.EventList.AddObject(key, TObject(luaL_Ref(L, LUA_REGISTRYINDEX)));
+        end;
+      end else begin
+        // delete event
+        i:= sink^.EventList.IndexOf(key);
+        if i >= 0 then begin
+          sink^.EventList.Delete(i);
+          if sink^.EventList.Count = 0 then sink^.DetachEvent;
+        end;
+      end;
+    end else begin
+      // Set value
+      ChkErr(L, id.GetIDsOfNames(GUID_NULL, @ws, 1, GetUserDefaultLCID, @di), key);
+      Lua2Var(L, @v, 3);
+      diput := DISPID_PROPERTYPUT;
+      param.rgvarg := @v;
+      param.cArgs := 1;
+      param.rgdispidNamedArgs := @diput;
+      param.cNamedArgs := 1;
+      ChkErr(L, id.Invoke(di, GUID_NULL, GetUserDefaultLCID,
+       DISPATCH_PROPERTYPUT, param, nil, nil, nil), key);
+    end;
   end;
 end;
 
@@ -356,7 +426,7 @@ begin
 
   id:= IDispatch(TVarData(p^).vDispatch);
   i:= lua_tointeger(L, lua_upvalueindex(1));
-  lua_pushinteger(L, i+1);
+  {%H-}lua_pushinteger(L, i+1);
   lua_replace(L, lua_upvalueindex(1));
 
   VariantInit(TVarData({%H-}v));
@@ -407,48 +477,82 @@ begin
   Result:= 3;
 end;
 
-function gc(L : Plua_State) : Integer; cdecl;
+function gc({%H-}L : Plua_State) : Integer; cdecl;
+begin
+  Result:= 0;
+end;
+
+function gc_ID(L : Plua_State) : Integer; cdecl;
 var
   p: POleVariant;
-  id: IDispatch;
 begin
   Result:= 0;
   p:= lua_touserdata(L, 1);
   if TVarData(p^).vtype <> varDispatch then Exit;
-  id:= IDispatch(TVarData(p^).vDispatch);
-  id._Release;
+  IDispatch(TVarData(p^).vDispatch)._Release;
+  //p^:= Unassigned;
+end;
+
+function gc_ID_client(L : Plua_State) : Integer; cdecl;
+var
+  sink: ^TEventSink;
+begin
+  Result:= 0;
+  sink:= lua_touserdata(L, 1);
+  sink^.Free;
 end;
 
 procedure DoCreateActiveXObject(L : Plua_State; id: IDispatch);
 var
   p: POleVariant;
+  sink: ^TEventSink;
 begin
-  id._AddRef;
+  //id._AddRef;
   lua_newtable(L);
 
-  lua_newtable(L);
+  lua_newtable(L); // new metatable
+
+  lua_pushstring(L, '__gc');
+  lua_pushcfunction(L, @gc);
+  lua_settable(L, -3);
+
   lua_pushstring(L, FIELD_ID);
   p:= lua_newuserdata(L, SizeOf(OleVariant));
   VariantInit(TVarData(p^));
   p^:= id;
   if lua_getmetatable(L, -1) = 0 then lua_newtable(L);
   lua_pushstring(L, '__gc');
-  lua_pushcfunction(L, @gc);
+  lua_pushcfunction(L, @gc_ID);
   lua_settable(L, -3);
   lua_setmetatable(L, -2);
   lua_settable(L, -3);
+
   lua_pushstring(L, '__newindex');
   lua_pushcfunction(L, @NewIndex);
   lua_settable(L, -3);
+
   lua_pushstring(L, '__index');
   lua_pushcfunction(L, @Index);
   lua_settable(L, -3);
+
   lua_pushstring(L, '__pairs');
   lua_pushcfunction(L, @pairs);
   lua_settable(L, -3);
+
   lua_pushstring(L, '__ipairs');
   lua_pushcfunction(L, @pairs);
   lua_settable(L, -3);
+
+  lua_pushstring(L, FIELD_ID_CLIENT);
+  sink:= lua_newuserdata(L, SizeOf(TEventSink));
+  if lua_getmetatable(L, -1) = 0 then lua_newtable(L);
+  lua_pushstring(L, '__gc');
+  lua_pushcfunction(L, @gc_ID_client);
+  lua_settable(L, -3);
+  lua_setmetatable(L, -2);
+  lua_settable(L, -3);
+  sink^:= TEventSink.Create(L);
+
   lua_setmetatable(L, -2);
 end;
 
@@ -491,6 +595,144 @@ begin
   lua_setmetatable(L, -2);
 
   Result := 1;
+end;
+
+{ TInterfacedBase }
+
+function TInterfacedBase.QueryInterface(constref iid : tguid; out obj) : longint; stdcall;
+begin
+  if GetInterface(IID, Obj) then Result := 0 else Result := E_NOINTERFACE;
+end;
+
+function TInterfacedBase._AddRef: Integer; stdcall;
+begin
+  Result := -1;
+end;
+
+function TInterfacedBase._Release: Integer; stdcall;
+begin
+  Result := -1;
+end;
+
+{ TEventSink }
+
+function TEventSink.GetTypeInfoCount(out Count: Integer): HRESULT; stdcall;
+begin
+  Count:= 0;
+  Result:= S_OK;
+end;
+
+function TEventSink.GetTypeInfo(Index, LocaleID: Integer; out TypeInfo): HRESULT; stdcall;
+begin
+  Result:= E_NOTIMPL;
+end;
+
+function TEventSink.GetIDsOfNames(const IID: TGUID; Names: Pointer; NameCount,
+  LocaleID: Integer; DispIDs: Pointer): HRESULT; stdcall;
+begin
+  Result:= E_NOTIMPL;
+end;
+
+function TEventSink.Invoke(DispID: Integer; const IID: TGUID;
+  LocaleID: Integer; Flags: Word; var Params; VarResult, ExcepInfo,
+  ArgErr: Pointer): HRESULT; stdcall;
+var
+  //dispparams: TDispParams;
+  name: WideString;
+  name2: string;
+  i: Integer;
+begin
+  Result := S_FALSE;
+  if FInfo.GetDocumentation(DispId, @name, nil, nil, nil) <> S_OK then Exit;
+  if name = '' then Exit;
+  name2:= UTF8Encode(name);
+  name:= '';
+  i:= EventList.IndexOf(LowerCase(name2));
+  if i >= 0 then begin
+    lua_rawgeti(L, LUA_REGISTRYINDEX, Integer(EventList.Objects[i]));
+
+    // TODO: param & result
+    {
+    dispparams := TDispParams(Params);
+    //逆順に入れる
+    for i := dispparams.cArgs - 1 downto 0 do begin
+      try
+        v := OleVariant(dispparams.rgvarg^[i]);
+      except
+        VariantInit(TVarData(v));
+      end;
+    end;
+    }
+
+    lua_pcall(L, 0{dispparams.cArgs}, 0{result}, 0);
+  end;
+  Result := S_OK;
+end;
+
+constructor TEventSink.Create(aL: PLua_State);
+begin
+  inherited Create;
+  L:= aL;
+  EventList:= TStringList.Create;
+  EventList.Sorted:= True;
+  FInfo:= nil;
+  FCPoint:= nil;
+end;
+
+destructor TEventSink.Destroy;
+var
+  i: Integer;
+begin
+  for i:= 0 to EventList.Count-1 do
+    luaL_unref(L, LUA_REGISTRYINDEX, Integer(EventList.Objects[i]));
+  EventList.Free;
+  if IsEventSupport then
+    FCPoint.UnAdvise(FCookie);
+  inherited Destroy;
+end;
+
+procedure TEventSink.AttachEvent(server: IDispatch);
+var
+  cp_c: IConnectionPointContainer;
+  cp: IConnectionPoint;
+  enum: IEnumConnectionPoints;
+  fetched: LongInt;
+  num: LongWord;
+  iid: TIID;
+  ti: ITypeInfo;
+  tl: ITypeLib;
+  i: integer;
+begin
+  if server.QueryInterface(IID_IConnectionPointContainer, cp_c) <> S_OK then
+    Exit;
+  ChkErr(L, cp_c.EnumConnectionPoints(enum));
+  if Assigned(enum) then begin
+    enum.Reset;
+    while enum.Next(1, cp, @fetched) = S_OK do begin
+      if fetched = 1 then begin
+        if cp.GetConnectionInterface(iid) <> S_OK then continue;
+        if server.GetTypeInfo(0, 0, ti) <> S_OK then continue;
+        if ti.GetContainingTypeLib(tl, num) <> S_OK then continue;
+        if tl.GetTypeInfoOfGuid(iid, FInfo) <> S_OK then continue;
+        if cp.Advise(Self, FCookie) <> S_OK then continue;
+        FCPoint:= cp;
+      end;
+      Inc(i);
+      Break; // TODO
+    end;
+  end;
+end;
+
+procedure TEventSink.DetachEvent;
+begin
+  if Assigned(FCPoint) then begin
+    FCPoint.UnAdvise(FCookie);
+  end;
+end;
+
+function TEventSink.IsEventSupport: boolean;
+begin
+  Result:= Assigned(FCPoint);
 end;
 
 end.
