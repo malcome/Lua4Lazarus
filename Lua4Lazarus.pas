4d8b4cd31606dc2f536f3c9dfaf3255c05712bc4
Zaher Dirkey <zaherdirkey@yahoo.com>
2019-11-18 19:53:06 +0200

    Add Lazarus Package to easy use it, for paths.

---
+++ Lua4Lazarus.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit Lua4Lazarus;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  l4l_object, lua53, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('Lua4Lazarus', @Register);
+end.
